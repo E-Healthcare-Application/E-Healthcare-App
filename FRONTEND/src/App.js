74a9140df84bdd7ab921e516031e187dd650d8ac
Swapnil Ramdas Kanherkar <123960430+Swapnilkanherkar2002@users.noreply.github.com>
2025-02-09 22:42:43 +0530

    Base template for frontend added

---
+++ FRONTEND/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+ 
+      <div className="">
+
+        <Header title="E-Healthcare" />
+       
+        <Routes>
+          
+
+        </Routes>
+
+      </div>
+   
+  );
+}
+
+export default App;
